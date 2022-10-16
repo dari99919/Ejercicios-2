2aa13ebe5569db2523db44345aaba315b6bd416a
dari99919 <dariio.dalba@gmail.com>
2022-10-16 14:08:31 +0200

    creamos la clase y las funciones

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+import math
+
+class Punto:
+    
+    def _init_(self, x, y):
+        self.x=x
+        self.y=y
+
+    def _str_(self):
+        cadena = "(" + str(self.x) + "," + str(self.y) +")"
+        return cadena
+    
+    def cuadrante(self):
+        cuadrante = None
+        if self.x > 0 and self.y > 0:
+            cuadrante = 'Primer cuadrante'
+        elif self.x > 0 and self.y < 0:
+            cuadrante = 'Cuarto cuadrante'
+        elif self.x < 0 and self.y > 0:
+            cuadrante = 'Segundo cuadrante'
+        elif self.x < 0 and self.y < 0:
+            cuadrante = 'Tercer cuadrante'
+        elif self.x == 0 and self.y != 0:
+            cuadrante = 'Sobre el eje Y'
+        elif self.x != 0 and self.y == 0:
+            cuadrante = 'Sobre el eje X'
+        elif self.x == 0 and self.y == 0:
+            cuadrante = 'Punto de origen'
+        return cuadrante
